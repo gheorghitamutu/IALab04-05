b5f3f030bbf053f28f213b29e3d119573e2f6c28
Gheorghita Mutu <gmutu@bitdefender.com>
2019-10-28 16:15:05 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+    Minimax Algorithm Game
+        https://drive.google.com/file/d/0Byr72F6j0SDqUnZKVXJLdGdJX2tHLXY4UjhQZC1oUjhXZ1V3/view
+"""
+
+if __name__ == "__main__":
+    print("Initial commit")
